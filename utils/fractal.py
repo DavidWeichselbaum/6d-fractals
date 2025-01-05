8a54d8b5092b21b68667e50fbb6514bdc4af08c2
David Weichselbaum <david.t.weichselbaum@gmail.com>
2025-01-05 16:16:37 +0100

    Code cleanup

--- utils/fractal.py
+++ utils/fractal.py
@@ -3,21 +3,21 @@ from numba import njit, prange
 
 
 @njit(parallel=True)
-def compute_fractal(c_z_e_array, max_iterations=100, escape_radius=2):
+def compute_fractal(c_z_p_array, max_iterations=100, escape_radius=2):
     """
     Compute a fractal based on the formula z = z^e + c.
     """
-    height, width, _ = c_z_e_array.shape
+    height, width, _ = c_z_p_array.shape
     escape_counts = np.zeros((height, width), dtype=np.int32)
 
     for i in prange(height):  # parallelized
         for j in range(width):
-            c, z, e = c_z_e_array[i, j]
+            c, z, p = c_z_p_array[i, j]
             count = 0
             while abs(z) <= escape_radius and count < max_iterations:
                 if not z:
                     z = c
-                z = z**e + c
+                z = z**p + c
                 count += 1
             escape_counts[i, j] = count
 
